2457487761ca63d9bb94dba8806306a1bd5a2fab
Case Ploeg <case.ploeg@gmail.com>
2020-08-03 23:10:49 -0400

    Converted uploading solution files to read from stream and making a database entry instead of saving the file to the server.
    When a file gets uploaded it is initially split up line by line, blank
    lines and lines with only new line characters are dropped. Empty files
    should be rejected.
    
    Updated the database schema to store solutions, currently keeps track of
    author_id, saved lines to the solution, and time created

--- leetreview/upload.py
+++ leetreview/upload.py
@@ -1,6 +1,10 @@
 import os
-from flask import Blueprint, send_from_directory, flash, request, redirect, url_for, current_app
+import json
+from flask import Blueprint, g, render_template, send_from_directory, flash, request, redirect, url_for, current_app
 from werkzeug.utils import secure_filename
+
+from leetreview.auth import login_required
+from leetreview.db import get_db
 
 
 ALLOWED_EXTENSIONS = {'py'}
@@ -11,6 +15,7 @@ def allowed_file(filename):
             filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @bp.route('/', methods=['GET', 'POST'])
+@login_required
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
@@ -18,15 +23,41 @@ def upload_file():
             flash('No file part')
             return redirect(request.url)
         file = request.files['file']
-        # if user does not select file, browser alse
-        # submite an empty part without filename
+        # if user does not select file, browser also
+        # submits an empty part without filename
         if file.filename == '':
             flash('No selected file')
             return redirect(request.url)
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
-            file.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('upload.uploaded_file', filename=filename))
+
+            lines = []
+            for line in file.stream:
+                # need to decode the input as utf-8, so this
+                # is a requirement for file format
+                line = line.rstrip().decode("utf-8")
+                # ignoring blank lines / purely whitespace
+                if line:
+                    lines.append(line)
+            obj = json.dumps(lines)
+
+            # if the file wasn't completely new lines / whitespace
+            # create a entry in the database
+            if len(lines) != 0:
+                db = get_db()
+                db.execute(
+                    'INSERT INTO solution (lines, author_id)'
+                    ' VALUES (?, ?)',
+                    (obj, g.user['id'])
+                )
+                db.commit()
+                return redirect(url_for('upload.uploaded_file', filename=filename))
+                # return redirect(url_for('index'))
+            else:
+                flash('Empty File')
+
+
+
 
     return '''
     <!doctype html>
@@ -41,4 +72,12 @@ def upload_file():
 
 @bp.route('<path:filename>')
 def uploaded_file(filename):
-    return send_from_directory(current_app.config['UPLOAD_FOLDER'], filename)
+    db = get_db()
+    solutions = db.execute(
+        'SELECT s.id, lines, created, author_id'
+        ' FROM solution s JOIN user u ON s.author_id = u.id'
+        ' ORDER BY created DESC'
+    ).fetchall()
+    current_app.logger.info(solutions)
+    return render_template('upload/index.html', solutions=solutions)
+    # return send_from_directory(current_app.config['UPLOAD_FOLDER'], filename)
